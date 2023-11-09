c1a622c98fef4d2a6dce52c00fd51f3c8b2fadc0
damianf2022 <damianf2022@gmail.com>
2023-11-08 21:41:45 -0300

    first commit

--- sumar1/src/components/Servicios.jsx
+++ sumar1/src/components/Servicios.jsx
@@ -6,84 +6,68 @@ import { faHandshake, faRetweet, faUsers, faCogs, faWrench, faBolt } from '@fort
 
 const Servicios = () => {
     return (
-        <div className='contenedor-servicios'>
-            <h2>Servicios</h2>
-            <img className='linea' src="https://sumarservicios.com.ar/wp-content/uploads/2020/01/divider.jpg" alt="linea" /> <br />
-            <div className='prueba'>
-                <div className='marco'>
-                    <div className="logoservi">
-                        <FontAwesomeIcon icon={faHandshake}  style={{ paddingRight:'270px' }}/>
-                    </div>
-                    <div className="servicio-info">
-                        <h4 style={{ paddingLeft:'57px' }}>Ventas</h4>
-                    </div>
-                    <p>Ofrecemos atención preventa personalizada para brindar el mejor asesoramiento para sus proyectos.
-                    </p>
-                </div>
-                <br /><br />
+
+        <div className='grand-content'>
+            
+            <div className='servis'>
+                <h2>Servicios</h2>
+                <div class="linea-divisora"></div>
             </div>
-            <div className='prueba'>
-                <div className='marco'>
-                    <div className="logoservi">
-                        <FontAwesomeIcon icon={faRetweet}  style={{ paddingRight:'270px' }}/>
-                    </div>
-                    <div className="servicio-info">
-                        <h4 style={{ paddingLeft:'64px' }}>Alquiler</h4>
-                    </div>
-                    <p>Contamos con una moderna flota de alquiler de 180 equipos. Sometemos a exahutivos chequeos y controles de seguridad, a fin de garantizar su máxima operatividad.
-                    </p>
+
+            <div class="content">
+                <div class="logo">
+                    <FontAwesomeIcon icon={faHandshake} />
                 </div>
-                <br /><br />
-            </div>
-            <div className='prueba'>
-                <div className='marco'>
-                    <div className="logoservi">
-                        <FontAwesomeIcon icon={faUsers} style={{ paddingRight:'270px' }} />
-                    </div>
-                    <div className="servicio-info">
-                        <h4 style={{ paddingLeft:'70px' }}>DSE Electronics</h4>
-                    </div>
-                    <p>Representamos en Argentina de la firma de DSE Electronics, empresa líder mundial en fabricación de controladores para grupos electrógenos y motores, lógicas de transferencias y cargadores de baterías.</p>
+                <div class="titulo">
+                    <h2>Ventas</h2>
+                    <p>Ofrecemos atención preventa personalizada para brindar el mejor asesoramiento para sus proyectos.</p>
                 </div>
-                <br /><br />
             </div>
-            <div className='prueba'>
-                <div className='marco'>
-                    <div className="logoservi">
-                        <FontAwesomeIcon icon={faCogs} style={{ paddingRight:'270px' }} />
-                    </div>
-                    <div className="servicio-info">
-                        <h4  style={{ paddingLeft:'70px' }}>Servicio técnico</h4>
-                    </div>
-                    <p>Contamos con personal técnico capacitado para brindar una excelente calidad de servicio tanto sobre equipos fabricados por nosotros como equipos de otras marcas</p>
+            <div class="content">
+                <div class="logo">
+                    <FontAwesomeIcon icon={faRetweet} />
                 </div>
-                <br /><br />
-            </div>
-            <div className='prueba'>
-                <div className='marco'>
-                    <div className="logoservi">
-                        <FontAwesomeIcon icon={faWrench}  style={{ paddingRight:'270px' }} />
-                    </div>
-                    <div className="servicio-info">
-                        <h4  style={{ paddingLeft:'70px' }}>Instalaciones</h4>
-                    </div>
-                    <p>Realizamos instalaciones de grupos electrógenos, tanto nuestros como adquiridos a nuestros colegas, contamos con todas las medidas de seguridad.</p>
+                <div class="titulo">
+                    <h2>Alquileres</h2>
+                    <p>Contamos con una moderna flota de alquiler de 180 equipos. Sometemos a exahutivos chequeos y controles de seguridad, a fin de garantizar su máxima operatividad.</p>
                 </div>
-                <br /><br />
             </div>
-            <div className='prueba'>
-                <div className='marco'>
-                    <div className="logoservi">
-                        <FontAwesomeIcon icon={faBolt}  style={{ paddingRight:'270px' }} />
-                    </div>
-                    <div className="servicio-info">
-                        <h4  style={{ paddingLeft:'70px' }}>Repuestos</h4>
-                    </div>
-                    <p>Disponemos de amplio stock de repuestos para toda nuestra flota de equipos de fabricación propia como así también de las marcas que representamos.</p>
+            <div class="content">
+                <div class="logo">
+                    <FontAwesomeIcon icon={faUsers} />
                 </div>
-                <br /><br />
+                <div class="titulo">
+                    <h2>DSE Electronics</h2>
+                    <p>Representamos en Argentina de la firma de DSE Electronics, empresa líder mundial en fabricación de controladores para grupos electrógenos y motores, lógicas de transferencias y cargadores de baterías.</p>                </div>
             </div>
+            <div class="content">
+                <div class="logo">
+                    <FontAwesomeIcon icon={faCogs} />
+                </div>
+                <div class="titulo">
+                    <h2>Servicio técnico</h2>
+                    <p>Contamos con personal técnico capacitado para brindar una excelente calidad de servicio tanto sobre equipos fabricados por nosotros como equipos de otras marcas.</p>                </div>
+            </div>
+            <div class="content">
+                <div class="logo">
+                    <FontAwesomeIcon icon={faWrench} />
+                </div>
+                <div class="titulo">
+                    <h2>Instalaciones</h2>
+                    <p>Realizamos instalaciones de grupos electrógenos, tanto nuestros como adquiridos a nuestros colegas, contamos con todas las medidas de seguridad.</p>                </div>
+            </div>
+            <div class="content">
+                <div class="logo">
+                    <FontAwesomeIcon icon={faBolt} />
+                </div>
+                <div class="titulo">
+                    <h2>Repuestos</h2>
+                    <p>Disponemos de amplio stock de repuestos para toda nuestra flota de equipos de fabricación propia como así también de las marcas que representamos.</p>                </div>
+            </div>
+
         </div>
+
+
     )
 }
 
