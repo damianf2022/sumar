0f16fe9105f544ca31c82f444d6368475d05517a
damianf2022 <damianf2022@gmail.com>
2023-11-27 08:56:30 -0300

    first commit

--- sumar1/src/components/Navbar.jsx
+++ sumar1/src/components/Navbar.jsx
@@ -1,48 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React, { useEffect } from 'react';
 import { FaPhone, FaEnvelope, FaLinkedin, FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 import './styles/Navbar.css';
 
 const Navbar = () => {
+    useEffect(() => {
+        // Verifica si el ancho de la ventana es menor o igual a 768px (puedes ajustar este valor según tus necesidades)
+        const isMobile = window.innerWidth <= 768;
+
+        // Obtiene el enlace de Instagram
+        const instagramLink = document.getElementById('instagram-link');
+
+        // Define las URL de destino para móviles y escritorio
+        const mobileInstagramURL = 'https://m.instagram.com/sumar_cba/';
+        const desktopInstagramURL = 'https://www.instagram.com/sumar_cba/';
+
+        // Asigna la URL correcta según el tipo de dispositivo
+        instagramLink.href = isMobile ? mobileInstagramURL : desktopInstagramURL;
+    }, []);
 
     return (
         <div className='container'>
-
             <div className='phone'>
                 <FaPhone />
-                {/* <a href="https://api.whatsapp.com/send/?phone=5493517667501&text&type=phone_number&app_absent=0">
-                </a> */}
                 +54 9 3517 66-7501
             </div>
 
             <div className='mail'>
                 <FaEnvelope />
                 <a href="mailto:comercialcba@sumarservicios.com.ar">comercialcba@sumarservicios.com.ar</a>
-                {/* comercialcba@sumarservicios.com.ar */}
             </div>
 
             <div className='icons'>
-
                 <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
                     <FaLinkedin />
                 </a>
-
                 <a href="https://www.facebook.com/SumarenCordoba" target="_blank">
                     <FaFacebook />
                 </a>
-                <a href="https://www.instagram.com/sumar_cba/" target="_blank">
+                <a id="instagram-link" href="#" target="_blank">
                     <FaInstagram />
-
                 </a>
-                {/* <a href="https://www.facebook.com/SumarenCordoba" target="_blank">
-
-                    <FaTwitter />
-                </a> */}
-
             </div>
-
         </div>
     );
 }
--- sumar1/src/components/Whatsapp.jsx
+++ sumar1/src/components/Whatsapp.jsx
@@ -52,18 +52,18 @@ const Whatsapp = () => {
   return (
     <div>
       {/* <AttentionSeeker effect="shakeX" style="" delay={2000}> */}
-        <style>{styles}</style>
+      <style>{styles}</style>
 
-        <a href="https://api.whatsapp.com/send/?phone=5493517667501&text&type=phone_number&app_absent=0" className="whatsapp-link">
-          <img src={iconwat} alt="WhatsApp" className="whatsapp-icon" />
-        </a>
+      <a href="https://api.whatsapp.com/send/?phone=5493517667501&text&type=phone_number&app_absent=0" className="whatsapp-link">
+        <img src={iconwat} alt="WhatsApp" className="whatsapp-icon" />
+      </a>
       {/* </AttentionSeeker> */}
-    
+
       {/* <AttentionSeeker effect="shakeX" style="" delay={3000}> */}
-        <style>{styles}</style>
-        <a href="https://api.whatsapp.com/send?phone=TUNUMEROTELEFONO" className="email-link">
-          <img src={email_icon} alt="Email" className="email-icon" />
-        </a>
+      <style>{styles}</style>
+      <a href="mailto:comercialcba@sumarservicios.com.ar " className="email-link">
+        <img src={email_icon} alt="Email" className="email-icon" />
+      </a>
       {/* </AttentionSeeker> */}
 
     </div>
