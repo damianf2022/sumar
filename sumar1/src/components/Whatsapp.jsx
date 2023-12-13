af47b9ae50a3d94f0f8b1b37402f401b81a0b4f2
damianf2022 <damianf2022@gmail.com>
2023-12-13 20:47:53 -0300

    first commit

--- sumar1/src/components/Whatsapp.jsx
+++ sumar1/src/components/Whatsapp.jsx
@@ -74,9 +74,9 @@ const Whatsapp = () => {
         <img src={email_icon} alt="Email" className="email-icon" />
       </a>
 
-      <div className="chatbot-link">
+      {/* <div className="chatbot-link">
         <Stack project="https://www.stack-ai.com/embed/79b1afa1-e824-4774-9ed8-171e87244404/02c2a5e4-d9b7-4590-b6f8-9928c4f56d5a/6579e625af130d25d522940f" className="chatbot-icon" />
-      </div>
+      </div> */}
     </div>
   );
 };
