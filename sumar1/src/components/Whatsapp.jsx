1ba867202cbd19bc2846d808f97f0cb77860e8a3
damianf2022 <damianf2022@gmail.com>
2023-12-13 20:23:05 -0300

    first commit

--- sumar1/src/components/Whatsapp.jsx
+++ sumar1/src/components/Whatsapp.jsx
@@ -1,10 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import iconwat from '../images/what_icon.png';
 import email_icon from '../images/email_icon.png';
-import { Link } from 'react-router-dom';
-
-import { AttentionSeeker } from 'react-awesome-reveal';
-import ChatBot from './ChatBot';
+import Stack from 'react-stackai';
 
 const Whatsapp = () => {
   const styles = `
@@ -22,7 +19,6 @@ const Whatsapp = () => {
       background: none;
       border: 3px solid #cccccc8d;
       box-shadow: 0 4px 8px rgb(255, 255, 255);
-  
     }
 
     .email-link {
@@ -39,58 +35,48 @@ const Whatsapp = () => {
       background: none;
       border: 3px solid #cccccc8d;
       box-shadow: 0 4px 8px rgb(255, 255, 255);
-  
     }
     .email-icon:hover {
         background-color: rgb(106, 177, 225);
-
     }
     .whatsapp-icon:hover {
         background-color: rgb(70, 236, 70);
-
     }
     .chatbot-link {
       position: fixed;
       bottom: 20px;
-      right: 20px;
+      left: calc(2px - 200px); /* Resta 200 px a la posición original */
       z-index: 9999;
     }
 
     .chatbot-icon {
-      width: 70px;
-      height: 70px;
+      width: 370px;
+      height: 370px;
       border-radius:50%;
       background: none;
       border: 3px solid #cccccc8d;
       box-shadow: 0 4px 8px rgb(255, 255, 255);
-  
     }
     .chatbot-icon:hover {
-      background-color: rgb(70, 236, 70);
-
-  }
+      /* Agrega estilos adicionales al hover si es necesario */
+    }
   `;
 
   return (
     <div>
-      {/* <AttentionSeeker effect="shakeX" style="" delay={2000}> */}
       <style>{styles}</style>
 
       <a href="https://api.whatsapp.com/send/?phone=5493517667501&text&type=phone_number&app_absent=0" className="whatsapp-link">
         <img src={iconwat} alt="WhatsApp" className="whatsapp-icon" />
       </a>
-      {/* </AttentionSeeker> */}
 
-      {/* <AttentionSeeker effect="shakeX" style="" delay={3000}> */}
-      <style>{styles}</style>
       <a href="mailto:comercialcba@sumarservicios.com.ar " className="email-link">
         <img src={email_icon} alt="Email" className="email-icon" />
       </a>
-      {/* </AttentionSeeker> */}
-      <Link to="/sumar/chatbot">
-        <div className="chatbot-icon">
-        </div>
-      </Link>
+
+      <div className="chatbot-link">
+        <Stack project="https://www.stack-ai.com/embed/79b1afa1-e824-4774-9ed8-171e87244404/02c2a5e4-d9b7-4590-b6f8-9928c4f56d5a/6579e625af130d25d522940f" className="chatbot-icon" />
+      </div>
     </div>
   );
 };
