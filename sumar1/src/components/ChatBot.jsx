7abc16b3fc925be46126013a8579a9adf1ec7314
damianf2022 <damianf2022@gmail.com>
2023-12-13 20:26:43 -0300

    first commit

--- sumar1/src/components/ChatBot.jsx
+++ sumar1/src/components/ChatBot.jsx
@@ -4,12 +4,11 @@ import Stack from 'react-stackai';
 function Chatbot() {
 
 
-  // Función para enviar un mensaje del usuario y obtener la respuesta del mode
   return (
     <div>
   <Stack project="https://www.stack-ai.com/embed/79b1afa1-e824-4774-9ed8-171e87244404/02c2a5e4-d9b7-4590-b6f8-9928c4f56d5a/6579e625af130d25d522940f" />
     </div>
-    
+
   );
 }
 
